01231d6681e8b1d194c5e7dbee45397345c283d5
WARUN01 <120333797+WARUNdev@users.noreply.github.com>
2023-06-05 16:22:31 +0530

    Create Change the value

---
+++ A-Z/03 Variables/ChangeValue.java
@@ -0,0 +1,9 @@
+//A new value you give to an existing variable will replace the old one if you do so:
+
+public class ChangeValue {
+    public static void main(String[] args) {
+        int myNum = 50;
+        myNum = 100;  // myNum is now 100
+        System.out.println(myNum);
+    }
+}
