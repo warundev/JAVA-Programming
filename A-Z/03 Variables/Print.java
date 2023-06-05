42a4a1486c9c6c2526947953906b8a8f16d159bc
WARUN01 <120333797+WARUNdev@users.noreply.github.com>
2023-06-05 16:15:29 +0530

    Create Display Variables

---
+++ A-Z/03 Variables/Print.java
@@ -0,0 +1,30 @@
+/*---Display Variables--- */
+
+/*--The println() method is often used to display variables.
+To combine both text and a variable, use the + character-- */
+
+public class Print {
+    public static void main(String[]args){
+        String firstName = "WARUNA ";
+        String lastName = "LIYANAPATHIRANA";
+        String fullName = firstName + lastName;
+
+        System.out.println(fullName);
+    }
+}
+
+
+/* ---The mathematical operator + is valid for numeric values (note that we are using int (integer) variables here): ---*/
+
+/*
+ public class Main {
+  public static void main(String[] args) {
+    int x = 5;
+    int y = 6;
+    System.out.println(x + y); // Print the value of x + y
+  }
+}
+
+ */
+
+ //x stores the value 5 , y stores the value 6 , Then we use the println() method to display the value of x + y, which is 11.
