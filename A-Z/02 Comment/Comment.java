4aee473d96e06561539d9d75d63ca488e79064bf
WARUN01 <120333797+WARUNdev@users.noreply.github.com>
2023-06-05 12:00:33 +0530

    Create Comments
    Comments can be used to explain Java code, and to make it more readable. It can also be used to prevent execution when testing alternative code.

---
+++ A-Z/02 Comment/Comment.java
@@ -0,0 +1,20 @@
+
+public class Comment {
+    public static void main(String[] args){
+        System.out.println("Comments in JAVA");//This is Single-line Comment
+
+        /*System.out.println("Multi-line Comments")*/  /*This is a multi-line comment*/
+    }
+}
+
+
+
+//---Single-line Comments--- 
+
+/*Single-line comments start with two forward slashes (//).
+Any text between // and the end of the line is ignored by Java */
+
+
+/*---Java Multi-line Comments--- */
+
+/*Multi-line comments start with /* and ends with */
