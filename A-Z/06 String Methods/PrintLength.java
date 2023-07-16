9b1d20be498016c8d3eaf393d2ac23bd77c58766
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-16 17:38:24 +0530

    Print String Length

---
+++ A-Z/06 String Methods/PrintLength.java
@@ -0,0 +1,14 @@
+/*
+ * System.out.println(hello.length());    Here "(hello.length())"" calculates the length of the String given by length by hello and gives it as output.
+ */
+
+public class PrintLength {
+    public static void main(String[]args) {
+
+        String hello = "Hello , How are you...!" ;
+        System.out.println(hello.length()); 
+
+    }
+}
+
+// Output : 23
