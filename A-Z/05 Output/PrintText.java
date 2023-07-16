8c7b6abcea73dd3feb3f5914564d3ca2c026c81c
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-16 15:58:36 +0530

    Create PrintText.java
    The "println()" method to output values or print text in Java.

---
+++ A-Z/05 Output/PrintText.java
@@ -0,0 +1,64 @@
+//you can use the println() method to output values or print text in Java:
+
+public class PrintText {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+      }
+}
+
+
+//You can add as many println() methods as you want. Note that it will add a new line for each method:
+
+//Like This: Main.java
+
+/*
+Code:--------------------------------------
+
+
+ public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World!");
+    System.out.println("Java is a popular programming language, created in 1995.");
+    System.out.println("This is Java Tutorial");
+  }
+}
+
+ */
+
+
+//Double Quotes:
+
+//It is necessary to enclose content in double quote marks while working with it.
+//A mistake results if you overlook the double quotes:
+
+/*
+Code:-------------------------------------
+ 
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("This sentence will work!");
+    System.out.println(This sentence will produce an error);
+  }
+}
+
+ */
+
+
+//The Print() Method :
+
+/*
+There is also a print() method, which is similar to println().
+
+The only difference is that it does not insert a new line at the end of the output: 
+
+Code:---------------------------------------
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.print("Hello World! ");
+    System.out.print("I will print on the same line.");
+  }
+}
+
+
+*/
