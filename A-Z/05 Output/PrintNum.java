d97b580a831f6416f512ee0f55ca4c6783a6adf4
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-16 16:48:24 +0530

    Print Numbers

---
+++ A-Z/05 Output/PrintNum.java
@@ -0,0 +1,24 @@
+//Print Numbers:
+/*You can also use the println() method to print numbers.*/
+
+public class PrintNum {
+    public static void main(String[] args) {
+        System.out.println(5);
+        System.out.println(800);
+        System.out.println(90000);
+      }
+}
+
+
+//The println() function allows you to execute mathematical operations as well:
+
+/*
+ 
+public class Main {
+  public static void main(String[] args) {
+    System.out.println(10 + 10);
+    System.out.println(2 * 5);
+  }
+}
+
+ */
