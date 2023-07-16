a0c74c6da4262a178009e7be9a1c86265860b973
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-16 20:06:40 +0530

    Create String To Lower Case
    This is the string method used to Lower Case a string.

---
+++ A-Z/PrintToLowerCase.java
@@ -0,0 +1,11 @@
+//This is the string method used to LowerCase a string:
+
+public class PrintToLowerCase {
+    public static void main (String[]args){
+        String hello = "HELLO WORLD";
+        System.out.println(hello.toLowerCase());
+    }
+}
+
+
+//Output : hello world
