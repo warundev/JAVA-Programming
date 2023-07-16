da71046fd31a37cc5290949fde45e89ca53ec7d2
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-16 17:36:50 +0530

    Create String Methods

---
+++ A-Z/06 String Methods/Print.java
@@ -0,0 +1,14 @@
+//Method 01 :
+
+public class Print {
+    public static void main(String[]args) {
+
+        String hello = "Hello , How are you...!" ;
+
+        System.out.println(hello);
+    }
+}
+
+//Output :
+
+/*    Hello,How are you....!     */
