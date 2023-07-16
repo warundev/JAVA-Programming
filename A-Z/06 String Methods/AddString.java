7e15a480c4c6a570eb6bacdd4569037c39fef27a
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-16 21:55:20 +0530

    Adding the String

---
+++ A-Z/06 String Methods/AddString.java
@@ -0,0 +1,11 @@
+//This shows the addition of String.
+
+public class AddString {
+    public static void main(String[]args){
+        String hello = "This is Java Programme";
+        System.out.println("Hello : "+ hello);
+    }
+}
+
+
+//Output :-- Hello : This is Java Programme
