c12a423cc4b85d97f69601feccc758313a32f4b2
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-16 18:24:30 +0530

    Create String To Upper Case
    This is the string method used to uppercase a string.

---
+++ A-Z/06 String Methods/PrintToUpperCase.java
@@ -0,0 +1,11 @@
+//This is the string method used to uppercase a string:
+
+public class PrintToUpperCase {
+    public static void main (String[]args){
+        String hello = "Hello World";
+        System.out.println(hello.toUpperCase());
+    }
+}
+
+
+//Output : HELLO WORLD
