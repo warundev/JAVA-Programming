fb79c26ca0d9340a0becd270afc2dec7fcc12173
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-07-16 22:07:08 +0530

    Print String Index
    This is used to get the Index of a word/letter in the String.

---
+++ A-Z/06 String Methods/StringIndexOf.java
@@ -0,0 +1,12 @@
+//This is used to get the Index of a word/letter in the String.
+
+public class StringIndexOf {
+    public static void main(String[]args) {
+        // initializing string
+        String hello = "Hello World , JAVA is best";
+        System.out.println(hello.indexOf("JAVA"));
+    }
+}
+
+
+//Output :-- 14
