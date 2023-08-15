5a82aafbc02181e7f243c817fca09f59760bb68d
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-08-15 12:26:47 +0530

    Addition of Numbers

---
+++ PRACTICAL SESSIONS/02-Addition/Main.java
@@ -0,0 +1,34 @@
+//Practical Number      : 02
+
+//------: Problem Specification : --------
+// Write a program to find the sum .
+
+//Code  :
+
+public class Main {
+    public static void main(String[] args) {
+        int num1, num2, add;
+
+        num1 = 10;
+        num2 = 20;
+
+        add = num1 + num2;
+
+        System.out.println("Output is : " + add);
+        System.out.println("The End");
+    }
+}
+
+// Output :
+/*
+ * Output is : 30
+ * The End
+ * 
+ */
+
+// Conclusion :
+
+/*
+ * This code is a simple Java program that calculates the sum of two numbers,
+ * num1 and num2, and then prints the result along with the message "The End".
+ */
