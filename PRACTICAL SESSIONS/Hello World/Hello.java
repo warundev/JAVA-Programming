a529ff783362d3c35a249d3c2098cfbb5359239c
WARUNA LIYANAPATHIRANA <120333797+WARUNdev@users.noreply.github.com>
2023-08-15 12:09:54 +0530

    Print Hello World

---
+++ PRACTICAL SESSIONS/Hello World/Hello.java
@@ -0,0 +1,25 @@
+//Practical Number      : 01
+
+//------: Problem Specification : --------
+//Print "Hello World in java "
+
+//Code  :
+
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello World...!");
+    }
+}
+
+// Output :
+/*
+ * 
+ * Hello World...!
+ * 
+ */
+
+// Conclusion :
+/*
+ * In this program we have learned how to print a string using the println()
+ * method of Java language.
+ */
